685fda402b9338d60ffa969be4505228e42f6011
zoekniskern <zxk3766@rit.edu>
2019-04-14 19:19:29 -0400

    Main Menu added and UI for placement/attack/gameover in scene

---
+++ Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel(string levelName) {
+        Debug.Log("Loaded " + levelName);
+        SceneManager.LoadScene(levelName);
+    }
+    }
