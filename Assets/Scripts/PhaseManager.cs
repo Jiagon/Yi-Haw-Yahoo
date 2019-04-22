3b3a5a666a943107b3ea591385483febdfc06b62
zoekniskern <zxk3766@rit.edu>
2019-04-22 11:45:56 -0400

    updated UI and tutorial

--- Assets/Scripts/PhaseManager.cs
+++ Assets/Scripts/PhaseManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public enum PhaseState
@@ -16,10 +17,21 @@ public class PhaseManager : MonoBehaviour
 
     private PhaseState currentState;
     EnemyManager eManager;
+    public GameObject screen_1;
+    public GameObject screen_2;
+    private Image screen;
+    public GameObject button_next;
+    public GameObject button_last;
 
     void Start(){
         SetGameState(PhaseState.Placement);
         eManager = GetComponent<EnemyManager>();
+        
+        //Tutorial components
+        screen_1 = GameObject.Find("screen_1");
+        screen_2 = GameObject.Find("screen_2");
+        button_next = GameObject.Find("tutorial_next");
+        button_last = GameObject.Find("tutorial_last");
     }
 
     public PhaseState GetCurrentState() {
@@ -35,6 +47,36 @@ public class PhaseManager : MonoBehaviour
     public void EnterPlacable() {
         eManager.ResetEnemies(15);
         SetGameState(PhaseState.Placement);
+    }
+    //HANDLING TUTORIAL
+    public void showTutorial() {
+        ToggleGameObjects("tutorialUI", true);
+        button_next.SetActive(true);
+        button_last.SetActive(false);
+    }
+    public void nextTutPage() {
+        print("nextTutCalled");
+        //switch screens
+        screen_1.SetActive(false);
+        screen_2.SetActive(true);
+        //switch buttons
+        button_next.SetActive(false);
+        button_last.SetActive(true);
+    }
+    public void lastTutPage() {
+        //switch screens
+        screen_1.SetActive(true);
+        screen_2.SetActive(false);
+        //switch buttons
+        button_next.SetActive(true);
+        button_last.SetActive(false);
+    }
+    public void closeTutorial() {
+        ToggleGameObjects("tutorialUI", false);
+        screen_1.SetActive(false);
+        screen_2.SetActive(false);
+        button_next.SetActive(false);
+        button_last.SetActive(false);
     }
     public void SetGameState(PhaseState phase) {
         currentState = phase;   //Set our new current game phase
